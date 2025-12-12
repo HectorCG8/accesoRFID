7fb2cf233d5b7821a14e7d8e1626d29a598505c7
HectorCG8 <111525386+HectorCG8@users.noreply.github.com>
2025-12-11 20:09:37 -0600

    Change database connection from MySQL to PostgreSQL

--- conexion.php
+++ conexion.php
@@ -1,18 +1,18 @@
 <?php
-$host = getenv("DB_HOST") ?: "bd-container";
+
+$host = getenv("DB_HOST") ?: "db";
+$port = getenv("DB_PORT") ?: "5432";
 $dbname = getenv("DB_NAME") ?: "acceso_rfid";
 $user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "123456789";
+$pass = getenv("DB_PASSWORD") ?: "123456789";
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
 } catch (PDOException $e) {
     die("ERROR DB: " . $e->getMessage());
 }
 
 ?>
-
-
-
-
